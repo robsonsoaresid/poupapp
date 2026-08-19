afa2fd127520ac3a4af02d9e492376219e582c35
robsonsoaresid <robemsuave@gmail.com>
2026-08-19 10:40:46 -0300

    Cria container peincipal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import logo from "./assets/logo.svg";
+import { Container } from "./Components/Container";
 
 function App() {
   return (
-    <>
+    <Container>
       <aside>
         <img src={logo} alt="" />
       </aside>
-    </>
+      <h1>Poupapp</h1>
+    </Container>
   );
 }
 
