cc14003aabdc7537fdd5abbc6bc7b9ad1bd12f10
robsonsoaresid <robemsuave@gmail.com>
2026-08-20 15:49:44 -0300

    Estrutura card, card.header e card.body

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { Aside } from "./Components/Aside";
 import { Container } from "./Components/Container";
 import { Main } from "./Components/Main";
 import { SearchInput } from "./Components/SearchInput";
-
+import { Typography } from "./Components/Typography";
 
 function App() {
   return (
     <Container>
       <Aside />
       <Main>
-        <SearchInput placeholder="Procure seu dinheiro"/>
-        <h1>Olá Rbson!</h1>
+        <SearchInput />
+        <div>
+        <Typography variant="h1">
+          Olá, Vinny!
+        </Typography>
+        <Typography variant="body">
+          Veja como estão suas finanças hoje.
+        </Typography>
+        </div>
       </Main>
     </Container>
   );
