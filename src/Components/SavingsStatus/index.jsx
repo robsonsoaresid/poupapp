2c65b22d87515e5cc9f9bc6275b03c24b5a4fa70
robsonsoaresid <robemsuave@gmail.com>
2026-08-21 19:57:55 -0300

    Cria barra de progresso

--- src/Components/SavingsStatus/index.jsx
+++ src/Components/SavingsStatus/index.jsx
@@ -1,12 +1,14 @@
 import { IconSavings } from '../icons'
+import { ProgressBar } from '../ProgressBar'
 import styles from './savingsstatus.module.css'
-
+            // status da poupança
 export const SavingsStatus = ({percent}) => {
     return (
         <div className={styles.container}>
             <p>
                 <IconSavings /> Economizar
             </p>
+            <ProgressBar percent={percent} />
         </div>
     )
 }
--- src/Components/SearchInput/index.jsx
+++ src/Components/SearchInput/index.jsx
@@ -1,6 +1,6 @@
 import { IconSearch } from '../icons'
 import styles from './searchinput.module.css'
-
+          // Campo de Pesquisa
 export const SearchInput = (props) => {
     return (
         <div className={styles.container}>
